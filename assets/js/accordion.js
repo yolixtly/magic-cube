022546010996159636e234b064a71d10e8d638eb
yolixtly <yolixtly_13@hotmail.com>
2020-01-22 20:08:46 -0800

    toggle Accordion complete

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,14 +1,31 @@
 var acc = document.getElementsByClassName("accordion");
+var panel = document.getElementsByClassName('panel');
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
+
+    acc[i].onclick = function () {
+        // First Remove the Active and Show from all the panels
+        const setClasses = !this.classList.contains('active');
+        setClass(acc, 'active', 'remove');
+        setClass(panel, 'show', 'remove');
+
+        // If its not active and was clicked, active it and show panel
+        if (setClasses) {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
         }
-    });
+    }
+}
+
+/**
+ * Sets or Removes classes to a list of dom elements
+ * @param {*} els
+ * @param {*} className
+ * @param {*} fnName
+ */
+function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
+    }
 }
