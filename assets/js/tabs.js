03e0498525bad68649ac1dab4877c0f1d01ed9f7
yolixtly <yolixtly_13@hotmail.com>
2020-01-22 22:35:57 -0800

    fix active tab

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,12 +1,15 @@
-function openTab(tabNumber) {
+function openTab(tabNumber, buttonId) {
     let i;
-    const x = document.getElementsByClassName("tab-body");
-    for (i = 0; i < x.length; i++) {
-        x[i].classList.remove("show");
+    const tabBody = document.getElementsByClassName("tab-body");
+    const tabButton = document.getElementsByClassName("tab");
+    for (i = 0; i < tabBody.length; i++) {
+        tabBody[i].classList.remove("show");
+        tabButton[i].classList.remove("active");
     }
 
+    const button = document.getElementById(buttonId);
+    button.classList.add('active');
     const element = document.getElementById(`tab${tabNumber}`);
-    console.log(element.classList);
     element.classList.add("show");
     console.log(element.classList);
 }
