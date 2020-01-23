42534cf45bd613405d4be795569b7623f558be0b
yolixtly <yolixtly_13@hotmail.com>
2020-01-22 22:09:31 -0800

    weather complete

---
+++ assets/js/weather.js
@@ -0,0 +1,31 @@
+function getTemperature(cityName) {
+    $.ajax({
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial`,
+        type: 'GET',
+        dataType: 'JSON',
+        data: {
+            appid: 'ed71690cc8140b9596d75e6f7e359f75'
+        }
+    })
+        .done((data) => {
+            // Remove Placeholder
+            const placeholder = document.getElementById("weather-placeholder");
+            if (placeholder) {
+                placeholder.remove();
+            }
+
+            // Set the City Name
+            const cityTag = document.getElementById("cityName");
+            cityTag.innerHTML = cityName;
+            // Set the Temperature
+            const tempTag = document.getElementById("temperature");
+            tempTag.innerHTML = data.main.temp;
+
+            //Show Weather Card
+            const weatherCard = document.getElementById("weather");
+            weatherCard.classList.add('show');
+        })
+        .fail((error) => {
+            console.error('error', error);
+        });
+}
